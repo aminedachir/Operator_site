eb22fa1025f70bba43c2b8ae76bbcddadca0bc70
aminedachir <dachiramine110@gmail.com>
2021-04-19 18:54:45 +0100

    edit sign in page

--- forms.py
+++ forms.py
@@ -6,4 +6,7 @@ class loginForm(FlaskForm):
 	firstname = StringField("First Name", validators=[DataRequired()])
 	lastname = StringField("Last name", validators=[DataRequired()])
 	password = PasswordField("password", validators=[DataRequired()])
+	confirm_password = PasswordField("confirm password", validators=[DataRequired()])
+	email = StringField("e-mail", validators=[DataRequired()])
+	confirm_email = StringField("confirm_email", validators=[DataRequired()])
 	acces = SubmitField("Submit")
