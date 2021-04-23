bcc24b498118aa33c0b80551f9307ef13a6bf5b9
aminedachir <dachiramine110@gmail.com>
2021-04-23 18:10:34 +0100

    add table

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 class user(db.Model):
@@ -6,8 +7,15 @@ class user(db.Model):
 	lastname = db.Column(db.String(120))
 	email = db.Column(db.String(120))
 	confirm_email = db.Column(db.String(120))
-	password = db.Column(db.String(120))
-	confirm_password = db.Column(db.String(120))
+	password_hash = db.Column(db.String(128))
+	confirm_password_hash = db.Column(db.String(128))
 
 	def __repr__(self):
 		return f"<user {self.firstname}>"
+
+
+class post(db,Model):
+	id = db.Column(db.Integer, primary_key=True)
+	body = db.Column(db.String(140))
+	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+	user_id = db.Column(db.Integer, db.Foreignkey('user.id'))
