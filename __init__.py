260d47479d7bbc7326a43cf50130c36f1ca80373
aminedachir <dachiramine110@gmail.com>
2021-04-25 02:11:08 +0100

    error

--- __init__.py
+++ __init__.py
@@ -1,11 +1,13 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate 
+import os
+print('zzzzzzzzzzzzz',os.curdir)
 from config import Config
 
 app = Flask(__name__)
 
-app.config.from_object('Config')
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask , render_template, redirect, request
 from forms import loginForm
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate 
-from config import Config
 
 app = Flask(__name__)
 app.config.from_object('config')
